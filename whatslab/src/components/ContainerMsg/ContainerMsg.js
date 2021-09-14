f7dd5d1927622ad5fbffa6d26846bf3639f162e3
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-09-14 14:53:32 -0300

    Feito a estilização dos componentes usando styled-components

--- whatslab/src/components/ContainerMsg/ContainerMsg.js
+++ whatslab/src/components/ContainerMsg/ContainerMsg.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const DadosInput = styled.div`
+    display: flex;
+    justify-content: start;
+    border: 1px solid gray;
+    margin: 3px;
+    gap: 2px;
+`
+const InputNome = styled.input `
+    width: 20%;
+    border: 1px solid black;
+`
+
+const InputTexto = styled.input `
+    width: 70%;
+`
 
 class ContainerMsg extends React.Component {
     render() {
         return (
-            <div className="container-texto">
-                <input className='nome-usuario' />
-                <input className='mensagem-usuario' />
+            <DadosInput>
+                <InputNome placeholder="Nome do Usuário"  />
+                <InputTexto placeholder= "Digite uma mensagem..." />
                 <button> Enviar </button>
 
-            </div>
+            </DadosInput>
         )
     }
 }
