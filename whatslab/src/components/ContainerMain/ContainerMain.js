5c717b5d86ceb809961488f8421496ebc75463b6
Rofmassa <rodrigo.sm89@gmail.com>
2021-09-14 11:58:02 -0300

    Rodrigo - Mudando Componentes Funcionais para Classes

--- whatslab/src/components/ContainerMain/ContainerMain.js
+++ whatslab/src/components/ContainerMain/ContainerMain.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function ContainerPrincipal(props) {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+class ContainerPrincipal extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
 
 export default ContainerPrincipal;
